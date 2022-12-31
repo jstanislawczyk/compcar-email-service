f9f5d6683995d8b3ccd86fac6ee0a0b7ef547ca4
jstanislawczyk <jakubstanislawczyk98@gmail.com>
2022-12-31 11:20:48 +0100

    Lint

--- src/application.ts
+++ src/application.ts
@@ -78,8 +78,8 @@ export class Application {
     const consumer: Consumer = Consumer.create(consumerOptions);
 
     consumer.on('error', (error: Error) => {
-      Logger.log(`Message: ${error.message}`, LoggerLevel.ERROR)
-      Logger.log(`SQS Consumer error: ${JSON.stringify(error)}`, LoggerLevel.ERROR)
+      Logger.log(`Message: ${error.message}`, LoggerLevel.ERROR);
+      Logger.log(`SQS Consumer error: ${JSON.stringify(error)}`, LoggerLevel.ERROR);
     });
 
     consumer.on('processing_error', (error: Error) =>
